6070ad28d06ca60172336497a35ed4723c838a9e
print-livelongflame <s4091514@student.rmit.edu.au>
2026-04-20 16:15:51 +1000

    finished off making the key generation and had to implement gcd  and mod inverse manully to fullfill spefications... function generates unique private and public keys

--- Inventory.py
+++ Inventory.py
@@ -1,4 +1,40 @@
 import math
+# Helper functions needed to calculate the private and public keys
+# GCD find the greatest common divisor between two variables and returns the result
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+# Mod inverse finds the inverse mod  using the extended euclidean algorithm
+# finds the number res such that (e * res) % phi  = 1 
+def mod_inverse(e, phi):
+    # We will need to keep reducing a and b unitl we find the gcd = 1
+    a = phi
+    b = e
+    # place hold variables used to track coefficeinets used to compute the inverse
+    x_0 = 0
+    x_1 = 1
+
+    while b != 0:
+        q = a // b
+        # Updating the remainders
+        temp_a = a
+        a = b
+        b = temp_a %b
+
+        
+        # Updating the coeffecints
+        temp_x0 = x_0
+        x_0 = x_1
+        x_1 = temp_x0 - q * x_1
+    
+    # Need to check if gcd(e,phi) != 1, then inverse does not exist
+    if a != 1: 
+        return None
+    # make the result a positive number
+    res = x_0 % phi
+    return res
 # We can create an object class where each invertory will have there values for p,q and e respectfully
 class Inventory:
     # Initialises the object
@@ -17,7 +53,23 @@ class Inventory:
         n = self.p  * self.q
         # computing phi
         phi = (self.p-1) * (self.q-1)
-        #todo: need to finish off key generation
+
+        # We first need to validate and choose e such that e is a co-prime to phi and is not dibisble by e. So e is 1 < e < phi. 
+        #  In other words gcd(e,phi) = 1
+        if gcd(self.e,phi) != 1:
+            while gcd(self.e , phi) !=1:
+                # We +2 to e to increase to next prime
+                self.e +=2
+
+         
+        # finding d with mod inverse
+        d = mod_inverse(self.e, phi)
+
+        self.public_key = (n,self.e)
+        self.private_key = (n,d)
+
+        return self.private_key,self.public_key
 
 
-        return -1, -1 
+
+
