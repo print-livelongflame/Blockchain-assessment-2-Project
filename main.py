00f1a2bd494304fe3412660d16b73ed9aaad3fc2
print-livelongflame <s4091514@student.rmit.edu.au>
2026-04-20 13:28:26 +1000

    added main.py file where most of our code will go. Also added todos from assessemnt doc to allow us to track progress and what needs to be done

---
+++ main.py
@@ -0,0 +1,34 @@
+'''
+Aurthors: Valentino Osorio Schwarz, Thomas McCarthy
+
+Note:
+For every task you do just add your name next to task. E.g. 1. Build this thing (Valentino).
+This should help us know who is working on what. 
+
+Also leave comments so other person knows what is happing within your code :)
+
+'''
+
+
+# ------- Task 1 --------
+# Todo: • Initialise the cryptographic parameters required for digital signature operations using the values provided in the List of Keys document.
+# Todo: • Derive any additional key components required for the digital signature process from the provided parameters, and ensure all required values are explicitly defined in your code.
+# Todo: •  Implement a mechanism that enables an inventory node to digitally sign a newly generated inventory record prior to submission.
+# Todo: • Implement a verification process that allows other inventory nodes to validate the authenticity and integrity of the received record before it proceeds to the consensus stage.
+# Todo: • In your report, explain how digital signatures contribute to secure record submission in a distributed inventory environment.
+
+# ------- Task 2 --------
+# Todo: • Select and justify an appropriate consensus mechanism for the given scenario, explaining how it ensures global agreement and handles inconsistent or malicious updates, including key trade-offs.
+# Todo: • Implement the selected consensus mechanism (in a simplified form) to determine whether a newly submitted record should be accepted or rejected.
+# Todo: • Ensure that all inventory nodes reach a consistent decision before the record is stored locally.
+# Todo: • After a successful consensus outcome, store the accepted record in each inventory node’s local database.
+
+# ------- Task 3 --------
+# Todo: • Initialise the cryptographic parameters required to support multi-signature-based verification and secure response delivery using the values provided in the List of Keys document.
+# Todo: • Implement a mechanism that allows an authorised external user to submit a query request to the  distributed inventory system.
+# Todo: • Implement a multi-signature-based verification process in which inventory nodes collectively approve the queried result before it is returned.
+# Todo: • Implement a mechanism that protects the approved response before transmission and allows the  querying user to recover and verify the returned information.
+# Todo: • Your implementation must demonstrate both protection of the response and successful recovery of the protected data at the user side.
+# Todo: • In your report, explain how multi-signature verification and secure delivery mechanisms improve trust, accountability, and confidentiality in distributed inventory queries.
+
+
