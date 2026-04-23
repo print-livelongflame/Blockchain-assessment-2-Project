4050ba63f3d69f71d2f53becc425a65c7357dd9e
LiveLongFlame <valentinoosorioschwarz@gmail.com>
2026-04-23 14:03:13 +1000

    changed the name and move the different parts into differetn files

---
+++ part2.py
@@ -0,0 +1,14 @@
+# ------ Part 2 ---- 
+'''
+Task3: 
+When an authorised external user submits a query to retrieve the quantity of a specific inventory item, the
+distributed inventory system must ensure that the returned result is jointly verified, authenticated, and
+securely delivered.
+'''
+# ------- Todos: 3 --------
+# Todo: • Initialise the cryptographic parameters required to support multi-signature-based verification and secure response delivery using the values provided in the List of Keys document.
+# Todo: • Implement a mechanism that allows an authorised external user to submit a query request to the  distributed inventory system.
+# Todo: • Implement a multi-signature-based verification process in which inventory nodes collectively approve the queried result before it is returned.
+# Todo: • Implement a mechanism that protects the approved response before transmission and allows the  querying user to recover and verify the returned information.
+# Todo: • Your implementation must demonstrate both protection of the response and successful recovery of the protected data at the user side.
+# Todo: • In your report, explain how multi-signature verification and secure delivery mechanisms improve trust, accountability, and confidentiality in distributed inventory queries.
--- patr1.py
+++ patr1.py
@@ -69,25 +69,3 @@ agree on whether a submitted record should be accepted.
 # Todo: • Implement the selected consensus mechanism (in a simplified form) to determine whether a newly submitted record should be accepted or rejected.
 # Todo: • Ensure that all inventory nodes reach a consistent decision before the record is stored locally.
 # Todo: • After a successful consensus outcome, store the accepted record in each inventory node’s local database.
-
-# ------ Part 2 ---- 
-'''
-Task3: 
-When an authorised external user submits a query to retrieve the quantity of a specific inventory item, the
-distributed inventory system must ensure that the returned result is jointly verified, authenticated, and
-securely delivered.
-'''
-# ------- Todos: 3 --------
-# Todo: • Initialise the cryptographic parameters required to support multi-signature-based verification and secure response delivery using the values provided in the List of Keys document.
-# Todo: • Implement a mechanism that allows an authorised external user to submit a query request to the  distributed inventory system.
-# Todo: • Implement a multi-signature-based verification process in which inventory nodes collectively approve the queried result before it is returned.
-# Todo: • Implement a mechanism that protects the approved response before transmission and allows the  querying user to recover and verify the returned information.
-# Todo: • Your implementation must demonstrate both protection of the response and successful recovery of the protected data at the user side.
-# Todo: • In your report, explain how multi-signature verification and secure delivery mechanisms improve trust, accountability, and confidentiality in distributed inventory queries.
-
-
-# ----- Creating UI -------
-'''
-    We will need to create a ui allowing users to go to the different tasks and adding a record with some information and where we can hash and sign the data and 
-    create a verification allowing us to ensure the data went through
-'''
