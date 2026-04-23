c74b00e124861a6370b05c9813dd01274da6b238
print-livelongflame <s4091514@student.rmit.edu.au>
2026-04-24 00:43:49 +1000

    added consensus method to help what needs to be done

--- part1.py
+++ part1.py
@@ -11,52 +11,20 @@ Note: When finsihed with the task just delete the "Todo" so we know its all done
 
 '''
 # ------ Part 1 ------ 
-'''
-Task1: 
-Each inventory node generates a new inventory record representing a recent item update. Before the record
-is broadcast to the distributed inventory system, the originating node must apply a digital signature to ensure
-the authenticity and integrity of the submitted data.
-'''
-# ---- Todos --------
-#• Initialise the cryptographic parameters required for digital signature operations using the values provided in the List of Keys document. (valentino)
 from Inventory import *
 
 # Initialising invertorys
-# Note: If u just want to use a variable you can do so by print(inver_A.p)
 inver_A =  Inventory(1210613765735147311106936311866593978079938707,1247842850282035753615951347964437248190231863, 815459040813953176289801, "A")
 inver_B = Inventory(787435686772982288169641922308628444877260947, 1325305233886096053310340418467385397239375379, 692450682143089563609787, "B")
 inver_C = Inventory(1014247300991039444864201518275018240361205111, 904030450302158058469475048755214591704639633,158749422015035388438057, "C" )
 inver_D = Inventory(1287737200891425621338551020762858710281638317,1330909125725073469794953234151525201084537607, 33981230465225879849295979, "D")
 
-
-# • Derive any additional key components required for the digital signature process from the provided parameters, and ensure all required values are explicitly defined in your code. (Valentino)
 # Initialsing keys
 private_key_A, public_key_A = inver_A.generate_keys()
 private_key_B, public_key_B = inver_B.generate_keys()
 private_key_C, public_key_C = inver_C.generate_keys()
 private_key_D, public_key_D = inver_D.generate_keys()
-
-# printing out public and private keys
-# inver_A.info_keys()
-# inver_B.info_keys()
-# inver_C.info_keys()
-# inver_D.info_keys()
-
-# adding and printing a new record 
-example_record1 = Record(4, "12", "18", "A")
-example_record2 = Record(3, "14", "18", "B")
-inver_A.add_record(example_record1)
-inver_B.add_record(example_record2)
-inver_A.info_records()
-inver_B.info_records()
     
-#•  Implement a mechanism that enables an inventory node to digitally sign a newly generated inventory record prior to submission.(valentino)
-# signing the first record example
-# print(inver_A.sign_record(inver_A.hash_record(0)))
-# : • Implement a verification process that allows other inventory nodes to validate the authenticity and integrity of the received record before it proceeds to the consensus stage. (valentino)
-inver_A.send_data_to(0,inver_B)
-inver_B.recevie_data_from("packageAtoB.txt",inver_A)
-inver_B.info_records()
 # Todo: • In your report, explain how digital signatures contribute to secure record submission in a distributed inventory environment.
 
 '''
@@ -66,6 +34,9 @@ agree on whether a submitted record should be accepted.
 '''
 # ------- Todos: --------
 # Todo: • Select and justify an appropriate consensus mechanism for the given scenario, explaining how it ensures global agreement and handles inconsistent or malicious updates, including key trade-offs.
+'''
+The consensus method we choose in the unanimous consensus as it verifys all data before goign in etc.. (write it in report lol)
+'''
 # Todo: • Implement the selected consensus mechanism (in a simplified form) to determine whether a newly submitted record should be accepted or rejected.
 # Todo: • Ensure that all inventory nodes reach a consistent decision before the record is stored locally.
 # Todo: • After a successful consensus outcome, store the accepted record in each inventory node’s local database.
